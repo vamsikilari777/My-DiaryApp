53accaca529947b1a674554f887673cc4c3450ae
vamsi kilari <vamsi kilari>
2023-12-25 12:34:29 +0530

    first commit

---
+++ UserRepository.java
@@ -0,0 +1,13 @@
+package com.twg.springboot.mydiary.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Query;
+
+import com.twg.springboot.mydiary.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	public User findByUsername(String username);
+	
+}
